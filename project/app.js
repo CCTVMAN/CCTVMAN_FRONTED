174d9252abbc53c18719329e43bd9b8c60884267
nottalk0922 <pmu42094@naver.com>
2022-11-28 15:44:04 +0900

    Update cctv.ejs

--- project/app.js
+++ project/app.js
@@ -1,23 +1,29 @@
 var express = require("express");
 var app = express();
-//var db_config = require(__dirname + "/config/database.js");
-//var conn = db_config.init();
-
-// db_config.connect(conn);
+var oracledb = require("oracledb");
+var db_info = {
+  host: "10.150.149.183",
+  port: "1521",
+  user: "c##madang",
+  password: "madang",
+  database: "MD_madang",
+};
 app.use(express.static(__dirname + "/public"));
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
+app.get("/db", function (req, res) { });
 app.get("/", function (req, res) {
   res.render("index.ejs");
 });
 app.get("/cctv", function (req, res) {
-  const sql = "select * from ";
-  conn.query(sql, function (err, rows, fields) {
-    if (err) console.log("oh Error..." + err);
-    else res.render("cctv.ejs", { list: rows });
-  });
+  // const sql = "select * from busanaddress";
+  // conn.query(sql, function (err, rows, fields) {
+  //   if (err) console.log("oh Error..." + err);
+  //   else res.render("cctv.ejs", { list: rows });
+  // });
+  res.render("cctv.ejs");
 });
 app.get("/school", function (req, res) {
   // const sql = "select * from ";
@@ -25,13 +31,15 @@ app.get("/school", function (req, res) {
   //   if (err) console.log("oh Error..." + err);
   //   else res.render("school.ejs", { list: rows });
   // });
-  res.render("school.ejs");
+  res.render("school.ejs", { list: { asdf: "asddddd" } });
 });
 app.get("/samchunpo", function (req, res) {
-  const sql = "select * from ";
-  conn.query(sql, function (err, rows, fields) {
-    if (err) console.log("oh Error..." + err);
-    else res.render("samchunpo.ejs", { list: rows });
-  });
+  // const sql = "select * from ";
+  // conn.query(sql, function (err, rows, fields) {
+  //   if (err) console.log("oh Error..." + err);
+  //   else res.render("samchunpo.ejs", { list: rows });
+  // });
+  res.render("samchunpo.ejs");
 });
+
 app.listen(3000, () => console.log("Server is running on port 3000..."));
