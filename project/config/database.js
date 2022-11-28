1e69b2aade89301b6ff928bcf17bf5048b8ac21d
sty0507 <styoung0507@gmail.com>
2022-11-28 16:16:01 +0900

    fix : connect db

--- project/config/database.js
+++ project/config/database.js
@@ -1,16 +1,20 @@
-var oracle = require("oracle");
+var oracle = require("oracledb");
 var db_info = {
   host: "10.150.149.183",
   port: "1521",
   user: "c##madang",
   password: "madang",
-  database: "MD_madang",
+  database: "project",
 };
 
 module.exports = {
   init: function () {
-    return oracle.createConnection(db_info);
+    return oracle.initOracleClient(db_info);
   },
+  // connect: function(){
+  //   let connection = oracle.getConnection(db_info)
+  //   if(connection){}
+  // }
   connect: function (conn) {
     conn.connect(function (err) {
       if (err) console.error("oracle connection error : " + err);
