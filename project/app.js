380178ff5fa05dd7ee45f93461ea6e0ae1c07a84
sty0507 <styoung0507@gmail.com>
2022-11-29 20:22:37 +0900

    school.ejs & cctv.ejs finish

--- project/app.js
+++ project/app.js
@@ -6,10 +6,10 @@ app.use(express.static(__dirname + "/public"));
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
-async function fetchData() {
+async function fetchData(sql) {
   try {
     const connection = await oracledb.getConnection({
-      user: "dbproject",
+      user: "c##project",
       password: "1234",
       connectString: "localhost/xe",
     });
@@ -24,21 +24,36 @@ app.get("/", function (req, res) {
   res.render("index.ejs");
 });
 app.get("/cctv", function (req, res) {
-  res.render("cctv.ejs");
-});
-app.get("/school", function (req, res) {
-  let sql = "select * from schooladdress";
+  let sql = "select * from busanaddress";
   fetchData(sql)
     .then((dbRes) => {
-      console.log(dbRes);
-      res.render("school.ejs", { list: dbRes });
+      res.render("cctv.ejs", { list: dbRes["rows"] });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.get("/school", function (req, res) {
+  let sql =
+    "select s.id, s.address, latitude, longitude from schooladdress s, busanaddress b where s.id = b.id";
+  fetchData(sql)
+    .then((dbRes) => {
+      res.render("school.ejs", { list: dbRes["rows"] });
     })
     .catch((err) => {
       console.log(err);
     });
 });
 app.get("/samchunpo", function (req, res) {
-  res.render("samchunpo.ejs");
+  let sql = "select * from sachancctv";
+  fetchData(sql)
+    .then((dbRes) => {
+      console.log(dbRes["rows"]);
+      res.render("samchunpo.ejs", { list: dbRes["rows"] });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.listen(3000, () => console.log("Server is running on port 3000..."));
